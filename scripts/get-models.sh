db76bf3414265a66e71c4b5a97ef72ee8d8bd1ce
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2020-03-20 20:16:39 -0700

    Add script for downloading the models

--- scripts/get-models.sh
+++ scripts/get-models.sh
@@ -3,4 +3,22 @@
 # Clone sagmaker-tensorflow-serving-container 
 # and Microsoft's CameraTrap repos
 
-megadetector=https://lilablobssc.blob.core.windows.net/models/camera_traps/megadetector/saved_model_megadetector_v3_tf19.zip
+mdURL=https://lilablobssc.blob.core.windows.net/models/camera_traps/megadetector/saved_model_megadetector_v3_tf19.zip
+originalPBFile=megadetector_v3_tf19_saved_model.pb
+mdDir=megadetector
+modelPath="$PWD/models"
+
+# get cameratrap repo
+if [ -d "$modelPath"/"$mdDir" ]; then
+  echo -e "Directory "$modelPath"/"$mdDir" already exits, skipping ...\n"
+else 
+  echo -e "Downloading and unzipping megadetector model..."
+  cd "$modelPath" && \
+  curl -sS "$mdURL" > md.zip && \
+  unzip md.zip && rm md.zip && \
+  rm -r __MACOSX/ && \
+  pbFile=$(find . -name '*.pb')
+  pbDir=$(dirname "$pbFile")
+  echo -e "renaming .pb file: $pbFile..."
+  mv "$pbFile" "$pbDir/saved_model.pb"
+fi
