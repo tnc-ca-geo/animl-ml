c486748b3d43b9637d819c58c6549c6ebc8ff69c
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2020-11-09 20:52:50 -0800

    Download megadetector v4 in get-models.sh script

--- scripts/get-models.sh
+++ scripts/get-models.sh
@@ -3,27 +3,33 @@
 # Download megadetector model files
 
 # Megadetector v3
-mdURL=https://lilablobssc.blob.core.windows.net/models/camera_traps/megadetector/saved_model_megadetector_v3_tf19.zip
-originalPBFile=megadetector_v3_tf19_saved_model.pb
-
-# # Megadetector v4.1.0
-# mdURL=https://lilablobssc.blob.core.windows.net/models/camera_traps/megadetector/md_v4.1.0/md_v4.1.0_saved_model.zip
-# originalPbFile=md_v4.1.0_saved_model.pb
-
+md3Url=https://lilablobssc.blob.core.windows.net/models/camera_traps/megadetector/saved_model_megadetector_v3_tf19.zip
+# Megadetector v4.1.0
+md4Url=https://lilablobssc.blob.core.windows.net/models/camera_traps/megadetector/md_v4.1.0/md_v4.1.0_saved_model.zip
 mdDir=megadetector
 modelPath="$PWD/models"
 
 if [ -d "$modelPath"/"$mdDir" ]; then
   echo -e "Directory "$modelPath"/"$mdDir" already exits, skipping ...\n"
 else 
-  echo -e "Downloading and unzipping megadetector model..."
+  echo -e "Downloading and unzipping megadetector v3..."
   cd "$modelPath" && \
-  curl -sS "$mdURL" > md.zip && \
-  unzip md.zip && rm md.zip && \
+  curl -sS "$md3Url" > md3.zip && \
+  unzip md3.zip && rm md3.zip && \
   rm -r __MACOSX/ && \
+  mv saved_model_megadetector_v3_tf19 "$mdDir"
+  cd $mdDir
+  mv 1 3
+  cd 3
   pbFile=$(find . -name '*.pb')
-  pbDir=$(dirname "$pbFile")
   echo -e "renaming .pb file: $pbFile..."
-  mv "$pbFile" "$pbDir/saved_model.pb"
+  mv "$pbFile" "saved_model.pb"
+  cd ..
+  echo -e "Downloading and unzipping megadetector v4..."
+  curl -sS "$md4Url" > md4.zip && \
+  unzip md4.zip && rm md4.zip && \
+  mv saved_model 4
+  cd ..
   find . -name '*.DS_Store' -type f -delete
+  echo -e "Success"
 fi
