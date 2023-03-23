681ed95419bb0b16662681b7a7d4199139de1bb1
Ryan Avery <ryanbarryavery@gmail.com>
2023-03-23 11:14:06 -0700

    turn off yolov5 autoinstall

--- api/megadetectorv5/docker_mdv5.sh
+++ api/megadetectorv5/docker_mdv5.sh
@@ -1,2 +1,2 @@
 # $1 is the path to the dir holding the .mar file. there should be only one .mar file
-docker run -p 8080:8080 -v $1:/opt/ml/model torchserve-mdv5a:0.5.3-cpu serve
+docker run -it -p 8080:8080 -v $1:/opt/ml/model torchserve-mdv5a:0.5.3-cpu serve
