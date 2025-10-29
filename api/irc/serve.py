00b0c4bc0a18b5b5a5dd9ad54228f84e055c6f7e
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2025-10-29 11:55:19 -0700

    parse JSON request body and crop image

--- api/irc/serve.py
+++ api/irc/serve.py
@@ -7,6 +7,10 @@ import requests
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import uvicorn
+import base64
+from PIL import Image, ImageOps
+import io
+from ast import literal_eval
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -32,20 +36,21 @@ async def invoke(request: Request):
         logger.info(f"Content-Type: {request.headers.get('content-type')}")
 
         # Get raw request body
-        body = await request.body()
-        logger.info(f"Request body: {body.decode('utf-8')}")
-        input_data = json.loads(body)
-
-        # Validate required fields and set defaults
-        if 'image_data' not in input_data:
-            return JSONResponse(
-                status_code=400,
-                content={"error": "Input must contain 'image_data' field"}
-            )
+        # body = await request.body()
+        # logger.info(f"Request body: {body.decode('utf-8')}")
+        # input_data = json.loads(body.decode('utf-8'))
+        input_data = await request.json()
         
         # print key input parameters
         logger.info('input_data keys: %s', input_data.keys())
 
+        # Validate required fields
+        if 'image' not in input_data:
+            return JSONResponse(
+                status_code=400,
+                content={"error": "Input must contain 'image' field"}
+            )
+        
         # # Get  parameters with defaults
         # components = input_data.get('components', 'all')
         # if components not in ['all', 'classifier', 'detector']:
@@ -64,14 +69,40 @@ async def invoke(request: Request):
         #         content={"error": "batch_size must be an integer"}
         #     )
 
+        # Get bbox from request or use default [x_min, y_min, width, height]
+        bbox = input_data.get('bbox', [0, 0, 1, 1])
+        logger.info(f"bbox type: {type(bbox)}")
+        if isinstance(bbox, str):
+            bbox = literal_eval(input_data.get("bbox"))
+
+        logger.info(f"bbox: {bbox}")
+        image = input_data.get('image')
+        logger.info(f"image type: {type(image)}")
+
+        if isinstance(image, str):
+            # if the image is a string of bytesarray.
+            logger.info("Decoding base64 image string...")
+            image = base64.b64decode(image)
+
+        # If the image is sent as bytesarray
+        if isinstance(image, (bytearray, bytes)):
+            logger.info("Opening image from bytes...")
+            image = Image.open(io.BytesIO(image))
+
+            # always save as RGB for consistency
+            if image.mode != 'RGB':
+                image = image.convert(mode='RGB')
+            
+            # crop, resize, and convert to tensor
+            image = crop(image, bbox)
+            # image = self.image_processing(image)
+            # print(f"tensor shape fully processed: {image.shape}")
+
         # Create temporary file for the image
-        import base64
-        from PIL import Image
-        from io import BytesIO
 
         # Decode and save image
-        image_bytes = base64.b64decode(input_data['image_data'])
-        image = Image.open(BytesIO(image_bytes))
+        # image_bytes = base64.b64decode(input_data['image'])
+        # image = Image.open(io.BytesIO(image_bytes))
         temp_path = "/tmp/temp_image.jpg"
         image.save(temp_path)
 
@@ -150,6 +181,57 @@ async def invoke(request: Request):
             status_code=500,
             content={"error": str(e)}
         )
+
+# adapted from: 
+# https://github.com/microsoft/CameraTraps/blob/main/classification/crop_detections.py
+def crop(img, bbox_rel):
+    """
+    Crops an image to the tightest square enclosing each bounding box. 
+    This will always generate a square crop whose size is the larger of the 
+    bounding box width or height. In the case that the square crop boundaries 
+    exceed the original image size, the crop is padded with 0s.
+
+    Args:
+        img: PIL.Image.Image object, already loaded
+        bbox_rel: list or tuple of float, [ymin, xmin, ymax, xmax] all in
+            relative coordinates
+
+    Returns: cropped image
+    """
+
+    print(f"cropping image. original image size: {img.size}")
+
+    img_w, img_h = img.size
+    xmin = int(bbox_rel[1] * img_w)
+    ymin = int(bbox_rel[0] * img_h)
+    box_w = int((bbox_rel[3] - bbox_rel[1]) * img_w)
+    box_h = int((bbox_rel[2] - bbox_rel[0]) * img_h)
+
+    # expand box width or height to be square, but limit to img size
+    box_size = max(box_w, box_h)
+    xmin = max(0, min(
+        xmin - int((box_size - box_w) / 2),
+        img_w - box_w))
+    ymin = max(0, min(
+        ymin - int((box_size - box_h) / 2),
+        img_h - box_h))
+    box_w = min(img_w, box_size)
+    box_h = min(img_h, box_size)
+
+    # if box_w == 0 or box_h == 0:
+    #     tqdm.write(f'Skipping size-0 crop (w={box_w}, h={box_h}) at {save}')
+    #     return False
+
+    # Image.crop() takes box=[left, upper, right, lower]
+    crop = img.crop(box=[xmin, ymin, xmin + box_w, ymin + box_h])
+
+    if (box_w != box_h):
+        # pad to square using 0s
+        crop = ImageOps.pad(crop, size=(box_size, box_size), color=0)
+
+    print(f"cropped image size: {crop.size}")
+
+    return crop
 
 def main():
     """Run the server"""
