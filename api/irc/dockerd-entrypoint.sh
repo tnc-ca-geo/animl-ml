5992c42224560520f61431cb2f5644b5fe3a7b11
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2025-10-29 21:38:15 -0700

    wait for TFServing to be ready before starting FastAPI

--- api/irc/dockerd-entrypoint.sh
+++ api/irc/dockerd-entrypoint.sh
@@ -2,13 +2,18 @@
 set -e
 
 if [[ "$1" = "serve" ]]; then
-    # shift 1
-    # printenv
-    # ls /opt
     echo "Starting TensorFlow Serving..."
     tensorflow_model_server --rest_api_port=8501 --model_name=irc --model_base_path=/opt/ml/model/irc &
+    
+    # Wait for TensorFlow Serving to be ready
+    echo "Waiting for TensorFlow Serving to be ready..."
+    until curl -s http://localhost:8501/v1/models/irc > /dev/null; do
+        sleep 1
+    done
+    echo "TensorFlow Serving is ready."
+
     echo "Starting FastAPI server..."
     python serve.py
 else
-    eval "$@"
+    exec "$@"
 fi
