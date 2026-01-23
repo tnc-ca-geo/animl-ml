64ddc559279e06fd8509dcd4ac567043a1a8d13a
Jesse Leung <jessetleung@gmail.com>
2026-01-23 00:17:40 -0500

    fix: make model loading global instead of in ping endpoint

--- models/megadetectorv1000/serve.py
+++ models/megadetectorv1000/serve.py
@@ -4,13 +4,12 @@ import os
 import time
 import logging
 from PIL import Image
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Request
 from fastapi.exceptions import HTTPException
 import uvicorn
-from functools import cache
-from dataclasses import dataclass
 from megadetector.detection.pytorch_detector import PTDetector
 from megadetector.detection.run_detector import load_detector
+
 
 # Configure logging
 logging.basicConfig(
@@ -19,60 +18,47 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Model config and model
-@dataclass(frozen=True)
-class Model:
-    detector: PTDetector
-    detection_threshold: float
+# Init config
+detection_threshold = os.getenv("DETECTION_THRESHOLD", 0.0005)
+model_path = os.getenv(
+    "MODEL_PATH",
+    "/opt/ml/model/md_v1000.0.0-redwood.pt",
+)
 
 
-# Cache to keep dependencies in memory between lambda invocations
-@cache
-def load_model() -> Model:
-    try:
-        start_time = time.time()
-        detection_threshold = os.getenv("DETECTION_THRESHOLD", 0.0005)
-        model_path = os.getenv(
-            "MODEL_PATH",
-            "/opt/ml/model/md_v1000.0.0-redwood.pt",
+# Load model
+try:
+    start_time = time.time()
+    detection_threshold = float(detection_threshold)
+
+    logger.info(f"Loading model from {model_path}")
+
+    model = load_detector(model_path)
+    if not isinstance(model, PTDetector):
+        raise ValueError(
+            "Megadetector v1000 is a PTDetector.  Ensure model file is a .pt file"
         )
-        detection_threshold = float(detection_threshold)
 
-        logger.info(f"Loading model from {model_path}")
-
-        model = load_detector(model_path)
-        if not isinstance(model, PTDetector):
-            raise ValueError(
-                "Megadetector v1000 is a PTDetector.  Ensure model file is a .pt file"
-            )
-
-        load_time = time.time() - start_time
-        logger.info(f"Model loaded successfully in {load_time:.2f}s")
-
-        return Model(
-            detector=model,
-            detection_threshold=detection_threshold,
-        )
-    except Exception as e:
-        logger.error(f"Failed to initialize model: {e}")
-        raise
+    load_time = time.time() - start_time
+    logger.info(f"Model loaded successfully in {load_time:.2f}s")
+except Exception as e:
+    logger.error(f"Failed to initialize model: {e}")
+    raise
 
 
+# Setup FastAPI app
 app = FastAPI()
 
 
 @app.get("/ping")
-async def ping(model: Model = Depends(load_model)):
-    try:
-        if model.detector:
-            return {"status": "Healthy"}
-        raise HTTPException(status_code=500, detail="Failed to load model")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Unhealthy: {e}")
+async def ping():
+    # if the model load fails or the threshold is invalid,
+    # the container will have already crashed
+    return {"status": "Healthy"}
 
 
 @app.post("/invocations")
-async def invoke(request: Request, model: Model = Depends(load_model)):
+async def invoke(request: Request):
     """
     Run MegaDetector inference on an image.
 
@@ -83,6 +69,11 @@ async def invoke(request: Request, model: Model = Depends(load_model)):
     """
     request_start = time.time()
     try:
+        # These are checked and gracefully handled in the loading step
+        # This is just type assertions to help the lsp / editor
+        assert isinstance(model, PTDetector)
+        assert isinstance(detection_threshold, float)
+
         # Decode image
         decode_start = time.time()
         body = await request.body()
@@ -96,8 +87,8 @@ async def invoke(request: Request, model: Model = Depends(load_model)):
 
         # Run inference
         inference_start = time.time()
-        result = model.detector.generate_detections_one_image(
-            image, detection_threshold=model.detection_threshold
+        result = model.generate_detections_one_image(
+            image, detection_threshold=detection_threshold
         )
         inference_time = time.time() - inference_start
 
